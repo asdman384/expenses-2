e257ac731132b554f533cc63e8935dc00554d566
Oleg <asdman87@gmail.com>
2022-05-10 23:46:22 +0300

    angular update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,32 +11,22 @@ import { SecurityService } from './services/security.service';
 import { SheetsService } from './services/sheets.service';
 
 function initServices(sec: SecurityService, sheet: SheetsService) {
-    return () => sec.init().then(() => {
-        if (sec.isSignedIn)
-            sheet.init();
-    });
+  return () => sec.init().then(() => sec.isSignedIn && sheet.init());
 }
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        PageSettingsComponent,
-        PageHomeComponent
-    ],
-    imports: [
-        FormsModule,
-        BrowserModule,
-        ReactiveFormsModule,
-
-        AppRoutingModule
-    ],
-    providers: [
-        SecurityService,
-        SheetsService,
-        { provide: APP_INITIALIZER, useFactory: initServices, multi: true, deps: [SecurityService, SheetsService] },
-        { provide: StorageService, useClass: LocalStorageService },
-        LocalStorageService,
-    ],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent, PageSettingsComponent, PageHomeComponent],
+  imports: [FormsModule, BrowserModule, ReactiveFormsModule, AppRoutingModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initServices,
+      multi: true,
+      deps: [SecurityService, SheetsService],
+    },
+    { provide: StorageService, useClass: LocalStorageService },
+    LocalStorageService,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
